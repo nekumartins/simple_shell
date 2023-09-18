7f1dcda89571dda94caf96515240800b87e9d400
Favour <ofavourite001@gmail.com>
2023-09-18 17:42:46 +0100

    Make changes

---
+++ nafsh.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX 1024
+
+int main()
+{
+    char req[MAX];
+
+    while (1)
+    {
+        printf("nafsh$ ");
+        scanf(req);
+        // if (req == "q")
+        // {
+        //     break;
+        // }
+        // fflush(stdin);
+    }
+
+    return (0);
+}
